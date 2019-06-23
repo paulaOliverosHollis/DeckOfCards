e5617e422ec0ccb7d207ca899ee3f75c504d1628
Paula Oliveros Hollis <paulaoliverosc@gmail.com>
2019-06-22 21:08:56 -0400

    Hooked up the menu and cleaned up the code.
    DeckOfCards- I deleted the unnecessary local variables.
    
    GuessTheCardGame- I added a class which is going to be a game that makes the user guess if the next card is lower or higher. I was able to get the method Run() up and going which handles the menu and calling of the different user method choices.

---
+++ ClassPaulita/GuessTheCardGame.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+public class GuessTheCardGame
+{  
+    private bool IsValidInput(string input)
+    {
+        if (input == null || input.Length < 1)
+            return false;
+
+        char choice = input[0];
+
+        // if user chose to play.
+        if (choice == 'p' || choice == 'P')
+        {
+            return true;
+        }
+
+        // if user chose to read the instructions.
+        if (choice == 'i' || choice == 'I')
+        {
+            return true;
+        }
+
+        // if user chose to see the score board.
+        if (choice == 'b' || choice == 'B')
+        {
+            return true;
+        }
+
+        // if user chose to quit.
+        if (choice == 'q' || choice == 'Q')
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private string PrintMenu()
+    {
+        string spacing = "\n\t\t\t\t\t\t";
+
+        string menu = $"{spacing}Press P To Play" +
+                      $"{spacing}Press I For Instructions" +
+                      $"{spacing}Press B To See The High Score Board" +
+                      $"{spacing}Press Q To Quit";
+
+        Console.WriteLine(menu);
+
+        string userChoice = Console.ReadLine();
+
+        while (!IsValidInput(userChoice))
+        {
+            Console.WriteLine("\n\tOoooops! You pressed an invalid option. Please try again.");
+
+            Console.WriteLine(menu);
+
+            userChoice = Console.ReadLine();
+        }
+
+        return userChoice;
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            string userChoice = PrintMenu();
+
+            if (userChoice[0] == 'p' || userChoice[0] == 'P')
+            {
+                Play();
+            }
+
+            if (userChoice[0] == 'i' || userChoice[0] == 'I')
+            {
+                Instructions();
+            }
+
+            if (userChoice[0] == 'b' || userChoice[0] == 'B')
+            {
+                HighScoreBoard();
+            }
+
+            if (userChoice[0] == 'q' || userChoice[0] == 'Q')
+            {
+                return;
+            }
+        }
+    }
+
+    private void Play()
+    {
+        Console.WriteLine("PLAYING");
+    }
+
+    private void Instructions()
+    {
+        Console.WriteLine("PLAY MOTHER FUCKER");
+    }
+
+    private void HighScoreBoard()
+    {
+        Console.WriteLine("PAULA = 1000000000000 POINTS.");
+    }    
+}
