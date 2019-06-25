622eef6217d68c9612cc3c7b33e1b2bd8fc8c749
Paula Oliveros Hollis <paulaoliverosc@gmail.com>
2019-06-25 16:05:34 -0400

    Got he Play() method up and running at a basic level. Created two new private methods: GetUserInput() and IsInputValid(). Both of these methods were created to aid Play() in getting all the information it needs from the user.

--- ClassPaulita/GuessTheCardGame.cs
+++ ClassPaulita/GuessTheCardGame.cs
@@ -1,41 +1,44 @@
 ﻿using System;
 
-public class GuessTheCardGame
-{  
-    private bool UserChoseToPlay(char userChoice)
+namespace ClassPaulita
+{
+    public class GuessTheCardGame
     {
-        return userChoice == 'p' || userChoice == 'P';
-    }
 
-    private bool UserchoseToSeeInstructions(char userChoice)
-    {
-        return userChoice == 'i' || userChoice == 'I';
-    }
-
-    private bool UserChosetoSeeBoard(char userChoice)
-    {
-        return userChoice == 'b' || userChoice == 'B';
-    }
-
-    private bool UserChoseToQuit(char userChoice)
-    {
-        return userChoice == 'q' || userChoice == 'Q';
-    }
-
-    private bool IsValidInput(string input)
-    {
-        if (input == null || input.Length < 1)
-            return false;
-
-        char userChoice = input[0];
-
-        if (UserChoseToPlay(userChoice) || UserchoseToSeeInstructions(userChoice) || UserChosetoSeeBoard(userChoice) || UserChoseToQuit(userChoice))
+        private bool UserChoseToPlay(char userChoice)
         {
-            return true;
+            return userChoice == 'p' || userChoice == 'P';
         }
-       
-        return false;
-    }
+
+        private bool UserchoseToSeeInstructions(char userChoice)
+        {
+            return userChoice == 'i' || userChoice == 'I';
+        }
+
+        private bool UserChosetoSeeBoard(char userChoice)
+        {
+            return userChoice == 'b' || userChoice == 'B';
+        }
+
+        private bool UserChoseToQuit(char userChoice)
+        {
+            return userChoice == 'q' || userChoice == 'Q';
+        }
+
+        private bool IsValidInput(string input)
+        {
+            if (input == null || input.Length < 1)
+                return false;
+
+            char userChoice = input[0];
+
+            if (UserChoseToPlay(userChoice) || UserchoseToSeeInstructions(userChoice) || UserChosetoSeeBoard(userChoice) || UserChoseToQuit(userChoice))
+            {
+                return true;
+            }
+
+            return false;
+        }
 
         private string PrintMenu()
         {
@@ -68,34 +71,58 @@ public class GuessTheCardGame
             {
                 string userChoice = PrintMenu();
 
-            if (UserChoseToPlay(userChoice[0]))
-            {
-                Play();
-            }
+                if (UserChoseToPlay(userChoice[0]))
+                {
+                    Play();
+                }
 
-            if (UserchoseToSeeInstructions(userChoice[0]))
-            {
-                Instructions();
-            }
+                if (UserchoseToSeeInstructions(userChoice[0]))
+                {
+                    Instructions();
+                }
 
-            if (UserChosetoSeeBoard(userChoice[0]))
-            {
-                HighScoreBoard();
-            }
+                if (UserChosetoSeeBoard(userChoice[0]))
+                {
+                    HighScoreBoard();
+                }
 
-            if (UserChoseToQuit(userChoice[0]))
-            {
-                return;
+                if (UserChoseToQuit(userChoice[0]))
+                {
+                    return;
+                }
             }
         }
-    }
 
         private void Play()
         {
             DeckOfCards deck = new DeckOfCards();
-            Console.WriteLine(deck.Deal());
-            Console.Write("Is the next card lower or higher?: ");
-            string userInput = Console.ReadLine();
+            deck.Suffle();
+
+            Card previousCard = deck.Deal();
+            
+
+            while (deck.CardsLeftOnDeck > 0)
+            {
+                Card currentCard = deck.Deal();
+
+                char userInput = GetUserInput(previousCard);
+
+                if (previousCard.CardValue > currentCard.CardValue && userInput == '1')
+                {
+                    Console.WriteLine("Correct!");
+                }
+                else if (previousCard.CardValue < currentCard.CardValue && userInput == '2')
+                {
+                    Console.WriteLine("Correct!");
+                }
+
+                else
+                {
+                    Console.WriteLine("Incorrect!");
+                }
+
+                previousCard = currentCard;
+            }
         }
 
         private void Instructions()
@@ -106,6 +133,39 @@ public class GuessTheCardGame
         private void HighScoreBoard()
         {
             Console.WriteLine("PAULA = 1000000000000 POINTS.");
+        }
+
+        private bool IsInputValid(string userInput)
+        {
+            if (userInput.Length < 1 || userInput == null)
+            {
+                return false;
+            }
+
+            else if (userInput[0] == '1' || userInput[0] == '2')
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        private char GetUserInput(Card previousCard)
+        {
+            Console.WriteLine($"\nIs the next card higher or lower than {previousCard}?");
+            string userInput = Console.ReadLine();
+
+            while (!IsInputValid(userInput))
+            {
+                Console.WriteLine("Your answer is not valid. Please try again!");
+                Console.WriteLine($"Is the next card higher or lower than {previousCard}?");
+                userInput = Console.ReadLine();
+            }
+
+            return userInput[0];
         }
     }
 }
